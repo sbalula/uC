38c5f29910de7d568c1411c214e4c66164ca59e4
Samuel Balula <samuel.balula@tecnico.ulisboa.pt>
2015-09-17 19:41:45 +0100

    Added bare minimum main file

---
+++ start.X/main.c
@@ -0,0 +1,16 @@
+//Bare minimum example
+
+#define FCY 4*7372800 		//instruction frequency in Hz
+
+#include <p30F4011.h>               //PIC device
+#include <libpic30.h>               //dsPIC30 library
+#include <xc.h>                     //Compiler
+
+
+//Device configuration
+_FOSC(CSW_FSCM_OFF & XT_PLL16); //oscillator at 16x PLL
+_FWDT(WDT_OFF); //watchdog timer is off
+
+void main() {
+
+}
